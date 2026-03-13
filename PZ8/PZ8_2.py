2daaaf5fbb59c67a0eddccef33a403647bf56ed3
Kolos-Kolos <romamixaylov2008@mail.ru>
2026-03-13 08:47:14 +0300

    PZ8_2.py

--- PZ8/PZ8_2.py
+++ PZ8/PZ8_2.py
@@ -1,1 +1,20 @@
+#Дан словарь на 6 персон, найти и вывести их средний возраст. 
+people = {
+    "Андрей": 32,
+    "Виктор": 29,
+    "Максим": 18,
+    "Игорь": 25,
+    "Дмитрий": 30,
+    "Алексей": 24
+}
 
+print("Исходный словарь:", people)
+
+total_age = 0
+
+for age in people.values():
+    total_age += age
+
+average_age = total_age / len(people)
+
+print("Средний возраст:", round(average_age, 2))
